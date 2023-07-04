ad9f29b6e9819a94531d129aa76639b05e43dfa8
kyhinds <133097374+kyhinds@users.noreply.github.com>
2023-07-04 17:10:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,53 @@
 const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
 const { Pool } = require('pg');
+const app = express();
+const port = 3000;
 
-app.use(bodyParser.json());
-
+// PostgreSQL database connection configuration
 const pool = new Pool({
-    host: 'localhost',
-    user: 'username',
-    password: 'password',
-    database: 'movie_ratings',
-    port: 5432
+  user: 'postgres',
+  host: 'db', // The hostname 'db' corresponds to the service name in docker-compose.yml
+  database: 'movie_review',
+  password: 'postgres',
+  port: 5432
 });
 
+// Endpoint to get all reviewer pairs who rated the same movie
 app.get('/api/reviewerPairs', async (req, res) => {
-    try {
-        const queryResult = await pool.query(`
-            SELECT 
-                DISTINCT LEAST(R1.name, R2.name) AS name1,
-                GREATEST(R1.name, R2.name) AS name2
-            FROM 
-                ratings AS RA1
-            JOIN 
-                ratings AS RA2 ON RA1.movie_id = RA2.movie_id AND RA1.reviewer_id <> RA2.reviewer_id
-            JOIN 
-                reviewers AS R1 ON RA1.reviewer_id = R1.id
-            JOIN 
-                reviewers AS R2 ON RA2.reviewer_id = R2.id
-            ORDER BY 
-                name1, name2;
-        `);
-        res.status(200).json(queryResult.rows);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'An error occurred while processing your request.' });
-    }
+  const result = await pool.query(`
+    SELECT 
+      DISTINCT LEAST(R1.name, R2.name) AS reviewer1,
+      GREATEST(R1.name, R2.name) AS reviewer2
+    FROM 
+      ratings AS RA1
+    JOIN 
+      ratings AS RA2 ON RA1.movie_id = RA2.movie_id AND RA1.reviewer_id <> RA2.reviewer_id
+    JOIN 
+      reviewers AS R1 ON RA1.reviewer_id = R1.id
+    JOIN 
+      reviewers AS R2 ON RA2.reviewer_id = R2.id
+    ORDER BY 
+      reviewer1, reviewer2
+  `);
+  res.json(result.rows);
 });
 
+// Endpoint to get reviewers who rated a specific movie
+app.get('/api/movies/:id/reviewers', async (req, res) => {
+  const { id } = req.params;
+  const result = await pool.query(`
+    SELECT 
+      reviewers.name
+    FROM 
+      ratings
+    JOIN 
+      reviewers ON ratings.reviewer_id = reviewers.id
+    WHERE 
+      ratings.movie_id = $1
+  `, [id]);
+  res.json(result.rows);
+});
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
 });
